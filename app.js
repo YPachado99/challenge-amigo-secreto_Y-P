9772cd29f04a18a0dd26404d879922316e3e8f1e
Yonathan Isaias Pachado <121469429+YPachado99@users.noreply.github.com>
2025-03-17 22:32:43 -0300

    Agregando el array de amigos y la funcion agregar amigos

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo() {
+    let amigo = document.getElementById('nombreamigo');
+    let nombre = input.value.trim();
+    if (amigo === "") {
+        alert('Debes ingresar un nombre');
+        return;
+    }
+
+    amigos.push(nombre);
+    console.log("Lista de amigos: " + amigos);
+    input.value = "";
+}
